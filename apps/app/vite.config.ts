3f77cc419134f3518a18f568b2e682074418a62e
alexy-os <agorbunoff.ru@gmail.com>
2025-02-03 10:36:41 +0300

    vite

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,2 +1,22 @@
-import baseConfig from '@packages/typescript-config/vite-base.config.mjs';
-export default baseConfig; 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+}); 
